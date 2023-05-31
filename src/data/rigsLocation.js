be6e617f17962cd5c584ad19adeccad76a551fca
Pang Shi Hao <shp92.dev@gmail.com>
2023-05-31 12:03:44 +0800

    imp(locations): updated locations for new fair

--- src/data/rigsLocation.js
+++ src/data/rigsLocation.js
@@ -2,49 +2,49 @@ export const RIGS_LOCATIONS = [
   {
     id: 1,
     name: 'NAGA 8',
-    location: 'Bumi C, offshore Peninsular Malaysia',
-    top: '50px',
+    location: 'Carigali HESS',
+    top: '30px',
     left: '200px',
     contentAlignSide: 'right'
   },
   {
     id: 2,
     name: 'NAGA 4',
-    location: 'Temu-1, offshore Sarawak',
-    top: '120px',
-    left: '460px',
+    location: 'SPS',
+    top: '350px',
+    left: '175px',
     contentAlignSide: 'left'
   },
   {
     id: 3,
     name: 'NAGA 5',
-    location: 'Paprika-1, offshore Sarawak',
-    top: '115px',
-    left: '700px',
+    location: 'HESS',
+    top: '90px',
+    left: '270px',
     contentAlignSide: 'right'
   },
   {
     id: 4,
     name: 'NAGA 2',
-    location: 'Bayan, offshore Sarawak',
-    top: '180px',
-    left: '460px',
+    location: 'Roc Oil',
+    top: '200px',
+    left: '550px',
     contentAlignSide: 'left'
   },
   {
     id: 5,
     name: 'NAGA 6',
-    location: 'Nahara, offshore Sarawak',
-    top: '180px',
-    left: '690px',
+    location: 'Prepping',
+    top: '110px',
+    left: '800px',
     contentAlignSide: 'right'
   },
   {
     id: 6,
     name: 'NAGA 3',
-    location: 'Offshore Vietnam',
-    top: '0px',
-    left: '20px',
+    location: 'PTTEP',
+    top: '180px',
+    left: '690px',
     contentAlignSide: 'right'
   }
 ]
