e67f8a7f6e40e142e477ffb5119cb0628c40e250
Pang Shi Hao <shp92.dev@gmail.com>
2023-06-10 21:40:54 +0700

    imp(): fixed rigs map width and location

--- src/data/rigsLocation.js
+++ src/data/rigsLocation.js
@@ -3,7 +3,7 @@ export const RIGS_LOCATIONS = [
     id: 1,
     name: 'NAGA 8',
     location: 'Carigali HESS',
-    top: '30px',
+    top: '20px',
     left: '200px',
     contentAlignSide: 'right'
   },
@@ -11,15 +11,15 @@ export const RIGS_LOCATIONS = [
     id: 2,
     name: 'NAGA 4',
     location: 'SPS',
-    top: '350px',
-    left: '175px',
+    top: '300px',
+    left: '145px',
     contentAlignSide: 'left'
   },
   {
     id: 3,
     name: 'NAGA 5',
     location: 'HESS',
-    top: '90px',
+    top: '80px',
     left: '270px',
     contentAlignSide: 'right'
   },
@@ -27,24 +27,24 @@ export const RIGS_LOCATIONS = [
     id: 4,
     name: 'NAGA 2',
     location: 'Roc Oil',
-    top: '200px',
-    left: '550px',
+    top: '180px',
+    right: '400px',
     contentAlignSide: 'left'
   },
   {
     id: 5,
     name: 'NAGA 6',
     location: 'Prepping',
-    top: '110px',
-    left: '800px',
+    top: '80px',
+    right: '140px',
     contentAlignSide: 'right'
   },
   {
     id: 6,
     name: 'NAGA 3',
     location: 'PTTEP',
-    top: '180px',
-    left: '690px',
+    top: '160px',
+    right: '260px',
     contentAlignSide: 'right'
   }
 ]
