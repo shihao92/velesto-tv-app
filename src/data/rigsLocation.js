99e43db48392e782a33d17c202251f56f0add97e
Pang Shi Hao <shp92.dev@gmail.com>
2024-09-19 22:58:31 +0800

    [IMP] patch location for gaits and rigs

--- src/data/rigsLocation.js
+++ src/data/rigsLocation.js
@@ -2,49 +2,49 @@ export const RIGS_LOCATIONS = [
   {
     id: 1,
     name: 'NAGA 8',
-    location: 'Carigali HESS',
-    top: '20px',
+    location: 'Block A-18, MTJDA',
+    top: '40px',
+    left: '50px',
+    contentAlignSide: 'left'
+  },
+  {
+    id: 2,
+    name: 'NAGA 3',
+    location: 'Block 15-2/01, offshore Vietnam',
+    top: '0px',
     left: '200px',
     contentAlignSide: 'right'
   },
   {
-    id: 2,
-    name: 'NAGA 4',
-    location: 'SPS',
-    top: '300px',
-    left: '145px',
-    contentAlignSide: 'left'
-  },
-  {
     id: 3,
     name: 'NAGA 5',
-    location: 'HESS',
+    location: 'Block A-18, MTJDA',
     top: '80px',
-    left: '270px',
+    left: '230px',
     contentAlignSide: 'right'
   },
-  // {
-  //   id: 4,
-  //   name: 'NAGA 2',
-  //   location: 'Roc Oil',
-  //   top: '180px',
-  //   right: '400px',
-  //   contentAlignSide: 'left'
-  // },
+  {
+    id: 4,
+    name: 'NAGA 2',
+    location: 'Block D18, offshore Sarawak',
+    top: '180px',
+    right: '400px',
+    contentAlignSide: 'left'
+  },
   {
     id: 5,
-    name: 'NAGA 6 & NAGA 2',
-    location: 'Prep @ Labuan',
-    top: '80px',
-    right: '250px',
-    contentAlignSide: 'left'
+    name: 'NAGA 6',
+    location: 'Labuan Shipyard Engineering (SPS)',
+    top: '100px',
+    right: '-10px',
+    contentAlignSide: 'right'
   },
   {
     id: 6,
-    name: 'NAGA 3',
-    location: 'PTTEP',
-    top: '160px',
-    right: '260px',
-    contentAlignSide: 'right'
+    name: 'NAGA 4',
+    location: 'enroute Kinabalu field, offshore Sabah',
+    top: '80px',
+    right: '300px',
+    contentAlignSide: 'left'
   }
 ]
